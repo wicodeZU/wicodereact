dfe49f3b9f098083f2b0865e8e00a2c47c0e2493
serahshani <kerenketepela@gmail.com>
2021-12-02 18:29:42 +0300

    Feat: add mq

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
   return (
     <div className="header">
       <nav className="nav__menu">
         <div className="logo">
-            <h1>WIcode</h1>
+          <h1>WIcode</h1>
         </div>
         <div className="nav__items">
-            <Link to='/' className='nav__links'>Home</Link>
-            <Link to='/about' className='nav__links'>About</Link>
-            <Link to='/team' className='nav__links'>Team</Link>
-            <Link to='/contact' className='nav__links'>Contact</Link>
+          <Link to="/" className="nav__links">
+            Home
+          </Link>
+          <Link to="/about" className="nav__links">
+            About
+          </Link>
+          <Link to="/team" className="nav__links">
+            Team
+          </Link>
+          <Link to="/contact" className="nav__links">
+            Contact
+          </Link>
+        </div>
+        <div className="humberger">
+          <MenuIcon />
         </div>
       </nav>
     </div>
