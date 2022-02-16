8bc46a9afde097c45144d0ded1a1fe62e012873a
Carver <kerenketepela@gmail.com>
2022-02-16 18:57:43 +0300

    add a github link

--- src/Footer.js
+++ src/Footer.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import './Footer.css'
-import FacebookIcon from '@mui/icons-material/Facebook';
-import { GitHub, Twitter, LinkedIn } from '@mui/icons-material';
+import React from "react"
+import "./Footer.css"
+import FacebookIcon from "@mui/icons-material/Facebook"
+import { GitHub, Twitter, LinkedIn } from "@mui/icons-material"
 
 function Footer() {
-    return (
-        <div className='footer'>
-            <div className="social__links">
-                <FacebookIcon className='sl'/>
-                <Twitter className='sl' />
-                <GitHub className='sl' />
-                <LinkedIn className='sl' />
-            </div>
-            <div className="footer__texts">
-                <p>Tearms</p>
-                <p>faqs</p>
-                <p>sponsors</p>
-                <p>patners</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="social__links">
+        <FacebookIcon className="sl" />
+        <Twitter className="sl" />
+        <GitHub href="https://github.com/wicodeZU" className="sl" />
+        <LinkedIn className="sl" />
+      </div>
+      <div className="footer__texts">
+        <p>Tearms</p>
+        <p>faqs</p>
+        <p>sponsors</p>
+        <p>patners</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
